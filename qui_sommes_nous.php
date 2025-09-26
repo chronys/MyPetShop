8c3bd65eed1f5a5919a217179d11662d7cfe5c3d
DylanAmory <dylanamory@yahoo.com>
2025-09-26 16:19:43 +0200

    Add files via upload

---
+++ qui_sommes_nous.php
@@ -0,0 +1,77 @@
+<?php require_once "../inc/header.inc.php"; ?>
+<div class="middle">
+    <h1 id="titre_qsn">Qui sommes-nous ?</h1>
+
+    <p class="presentation">Nous sommes des développeurs web & mobile junior en cours formation, et ce site est un
+        projet de groupe (Workshop).</p>
+    <p class="presentation">Notre équipe est composé de quatres personnes : Bintou, Dylan, Hakim, Sabrina.</p>
+
+    <h2 id="titre_qsn2">Notre équipe</h2>
+    <section id="roles">
+        <div class="div_qsn">
+            <p class="sous-titre"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+                    fill="none" stroke="#000000" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-dog-icon lucide-dog">
+                    <path d="M11.25 16.25h1.5L12 17z" />
+                    <path d="M16 14v.5" />
+                    <path
+                        d="M4.42 11.247A13.152 13.152 0 0 0 4 14.556C4 18.728 7.582 21 12 21s8-2.272 8-6.444a11.702 11.702 0 0 0-.493-3.309" />
+                    <path d="M8 14v.5" />
+                    <path
+                        d="M8.5 8.5c-.384 1.05-1.083 2.028-2.344 2.5-1.931.722-3.576-.297-3.656-1-.113-.994 1.177-6.53 4-7 1.923-.321 3.651.845 3.651 2.235A7.497 7.497 0 0 1 14 5.277c0-1.39 1.844-2.598 3.767-2.277 2.823.47 4.113 6.006 4 7-.08.703-1.725 1.722-3.656 1-1.261-.472-1.855-1.45-2.239-2.5" />
+                </svg><span class="svg-espace"></span>BINTOU</p>
+            <div class="sous-div-roles">
+                <p class="roles"><u>Rôle :</u> maquettage, header et footer.</p>
+            </div>
+        </div>
+        <div class="div_qsn">
+            <p class="sous-titre"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+                    fill="none" stroke="#ffffff" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-dog-icon lucide-dog">
+                    <path d="M11.25 16.25h1.5L12 17z" />
+                    <path d="M16 14v.5" />
+                    <path
+                        d="M4.42 11.247A13.152 13.152 0 0 0 4 14.556C4 18.728 7.582 21 12 21s8-2.272 8-6.444a11.702 11.702 0 0 0-.493-3.309" />
+                    <path d="M8 14v.5" />
+                    <path
+                        d="M8.5 8.5c-.384 1.05-1.083 2.028-2.344 2.5-1.931.722-3.576-.297-3.656-1-.113-.994 1.177-6.53 4-7 1.923-.321 3.651.845 3.651 2.235A7.497 7.497 0 0 1 14 5.277c0-1.39 1.844-2.598 3.767-2.277 2.823.47 4.113 6.006 4 7-.08.703-1.725 1.722-3.656 1-1.261-.472-1.855-1.45-2.239-2.5" />
+                </svg><span class="svg-espace"></span>DYLAN</p>
+            <div class="sous-div-roles">
+                <p class="roles"><u>Rôle :</u> page de contact et page qui sommes-nous ?</p>
+            </div>
+        </div>
+        <div class="div_qsn">
+            <p class="sous-titre"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+                    fill="none" stroke="#be9883" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-dog-icon lucide-dog">
+                    <path d="M11.25 16.25h1.5L12 17z" />
+                    <path d="M16 14v.5" />
+                    <path
+                        d="M4.42 11.247A13.152 13.152 0 0 0 4 14.556C4 18.728 7.582 21 12 21s8-2.272 8-6.444a11.702 11.702 0 0 0-.493-3.309" />
+                    <path d="M8 14v.5" />
+                    <path
+                        d="M8.5 8.5c-.384 1.05-1.083 2.028-2.344 2.5-1.931.722-3.576-.297-3.656-1-.113-.994 1.177-6.53 4-7 1.923-.321 3.651.845 3.651 2.235A7.497 7.497 0 0 1 14 5.277c0-1.39 1.844-2.598 3.767-2.277 2.823.47 4.113 6.006 4 7-.08.703-1.725 1.722-3.656 1-1.261-.472-1.855-1.45-2.239-2.5" />
+                </svg><span class="svg-espace"></span>HAKIM</p>
+            <div class="sous-div-roles">
+                <p class="roles"><u>Rôle :</u> chef d'équipe (github et BDD).</p>
+            </div>
+        </div>
+        <div class="div_qsn">
+            <p class="sous-titre"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"
+                    fill="none" stroke="#d6beb1" stroke-width="1.75" stroke-linecap="round" stroke-linejoin="round"
+                    class="lucide lucide-dog-icon lucide-dog">
+                    <path d="M11.25 16.25h1.5L12 17z" />
+                    <path d="M16 14v.5" />
+                    <path
+                        d="M4.42 11.247A13.152 13.152 0 0 0 4 14.556C4 18.728 7.582 21 12 21s8-2.272 8-6.444a11.702 11.702 0 0 0-.493-3.309" />
+                    <path d="M8 14v.5" />
+                    <path
+                        d="M8.5 8.5c-.384 1.05-1.083 2.028-2.344 2.5-1.931.722-3.576-.297-3.656-1-.113-.994 1.177-6.53 4-7 1.923-.321 3.651.845 3.651 2.235A7.497 7.497 0 0 1 14 5.277c0-1.39 1.844-2.598 3.767-2.277 2.823.47 4.113 6.006 4 7-.08.703-1.725 1.722-3.656 1-1.261-.472-1.855-1.45-2.239-2.5" />
+                </svg><span class="svg-espace"></span>SABRINA</p>
+            <div class="sous-div-roles">
+                <p class="roles last_child"><u>Rôle :</u> pages produits.</p>
+            </div>
+        </div>
+    </section>
+</div>
+<?php require_once "../inc/footer.inc.php"; ?>
